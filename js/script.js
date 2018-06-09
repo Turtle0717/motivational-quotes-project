632d6148922d4907b60a761234ba1f1e765e4de1
Victor Chan <vchan0717@gmail.com>
2018-06-09 18:41:41 -0400

    Changes to script.js
    Added functionality to script.js that allows one quote to be displayed after pressing on the "Show another quote" button.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// Array to hold quotes
+var quotes = [
+    {
+        quote: "A person who never made a mistake never tried anything new.",
+        source: "Albert Einstein"
+    }
+];
+
+//Function to generate a random number and returns a random quote object
+function getRandomQuote(quotes) {
+    var randomNumber = Math.floor(Math.random());
+    return quotes[randomNumber];
+}
+
+//Function to to call getRandomQuote function and print quote.
+function printQuote() {
+    var quoteMessage = getRandomQuote(quotes);
+    var message = "<p class='quote'>" + quoteMessage.quote + "</p>" 
+    message += "<p class='source'>" + quoteMessage.source + "</p>";
+    message += "</p>";
+    document.getElementById('quote-box').innerHTML = message;
+}
